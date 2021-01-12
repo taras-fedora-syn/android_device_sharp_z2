5779dcd37c3ad1b0c1e905a296d52cd7809c78e2
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2021-01-12 12:41:49 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -26,9 +26,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
 	$(LOCAL_PATH)/configs/media_codecs_mediatek_video.xml:system/etc/media_codecs_mediatek_video.xml \
+	$(LOCAL_PATH)/configs/media_codecs_mediatek_audio.xml:system/etc/media_codecs_mediatek_audio.xml \
 	$(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
-
+	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+	$(LOCAL_PATH)/configs/media_profiles.xml_V1_0:system/etc/media_profiles.xml_V1_0
+	
 # Ramdisk
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
