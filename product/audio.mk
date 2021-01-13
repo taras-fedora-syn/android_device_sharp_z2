5d897df1a12d4e92f567681c1457bc10bb63fb10
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2021-01-13 16:01:00 +0200

    Update audio.mk

--- product/audio.mk
+++ product/audio.mk
@@ -10,7 +10,6 @@ PRODUCT_PACKAGES += \
     libtinyxml
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
