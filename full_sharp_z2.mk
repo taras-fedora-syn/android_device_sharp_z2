b76fa2013a65247c9e81c8aaca5e3a36bf6cde6d
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2021-02-05 17:37:54 +0200

    Update full_sharp_z2.mk

--- full_sharp_z2.mk
+++ full_sharp_z2.mk
@@ -21,6 +21,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="FS8002_00WW-user 6.0 MRA58K 00WW_1_360 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "SHARP/FS8002_00WW/TSP:6.0/MRA58K/00WW_1_360:user/release-keys"
+BUILD_FINGERPRINT := "alps/TSP_0001_FIH/TSP:6.0/MRA58K/0001_1_360:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
