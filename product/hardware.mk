b9924992d0c52896caccae9a3aeb2f4bbac0a601
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2021-02-13 07:11:53 +0200

    Update hardware.mk

--- product/hardware.mk
+++ product/hardware.mk
@@ -1,6 +1,5 @@
 # Hardware-specific permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/android.hardware.microphone.xml:system/etc/permissions/android.hardware.microphone.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
 
