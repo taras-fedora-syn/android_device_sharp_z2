0e49ff548c05a0d053f0bf5762dacbda6088470f
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2021-02-27 05:35:49 +0200

    Update audio.mk

--- product/audio.mk
+++ product/audio.mk
@@ -9,11 +9,6 @@ PRODUCT_PACKAGES += \
     libtinycompress \
     libtinyxml
 
-PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
-
 # Codecs
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
