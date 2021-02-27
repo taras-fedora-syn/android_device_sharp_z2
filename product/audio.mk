6c07badbc0f2b33b26f42066332bae8408881a32
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2021-02-27 06:18:17 +0200

    Update audio.mk

--- product/audio.mk
+++ product/audio.mk
@@ -14,7 +14,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
-    
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio_param/AudioParamOptions.xml:system/etc/audio_param/AudioParamOptions.xml \
     $(LOCAL_PATH)/audio_param/PlaybackACF_AudioParam.xml:system/etc/audio_param/PlaybackACF_AudioParam.xml \
